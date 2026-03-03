988d4ea4700df0c708f84731e4d1bbab42db4993
programador-cyberpunk <hugosolidsnake@gmail.com>
2026-03-03 18:24:37 -0300

    eh,tentei

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // CORREÇÃO: Passando os parâmetros que os construtores agora exigem
+
         Pizza p1 = new Pizza("Calabresa", 45.0, "10/2026", 0.8);
         Salgado s1 = new Salgado("Coxinha", 8.0, "05/2026", 0.2);
         Lanche l1 = new Lanche("X-Salada", 25.0, "04/2026", 0.4);
