b4f280091ef97a4fdaf71ac74105b01f0a9dd474
hugo <86883067+programador-cyberpunk@users.noreply.github.com>
2026-03-17 18:26:42 -0300

    Add files via upload

---
+++ Aula02/src/DesafioDev.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.NoSuchElementException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DesafioDev {
+   //vou arrumas as classes primeiro
+    class InitialData{
+
+    }
+    class Aluno{
+        public int Id;
+        public String Nome;
+        public String Email;
+        public String Curso;
+        public String Assinatura;
+    }
+    class Matriculas{
+        String Nome;
+        int CPF;
+        int Numero;// o numero da matricula no caso
+    }
+        String Status;
+       Id id = new id();
+      void NovoId(){
+
+      };
+    class Support{
+
+    }
+    class Curso{
+        String Titulo;
+        String Professor;
+        int duracao;
+        String Lvl;
+        String base;//referente ao nivel do curso
+        String patrao; //referente ao nivel do curso
+    }
+
+}
