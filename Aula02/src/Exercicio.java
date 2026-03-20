8e708256b979f714756fab80cbf5280b836d7857
hugo <86883067+programador-cyberpunk@users.noreply.github.com>
2026-03-20 18:33:49 -0300

    Add files via upload

---
+++ Aula02/src/Exercicio.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.Optional;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+public class Exercicio {
+    class Produto{
+        String nome;
+        double preco;
+        String catiguria;
+     String catiguria = "Livros; Eletros; Moveis;";
+    }
+    private class Teste {
+        public List<String> sfindFeaturedProductedNamesImperative() {
+            List<String> list = new ArrayList<>(catiguria);
+            for
+        }
+    }
+}
